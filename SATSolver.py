45291b4416c724f0275757faa47d13d0762ea161
RaymondKoopmanschap <raymond95@hotmail.nl>
2019-03-02 22:12:11 +0100

    linear regression added

--- SATSolver.py
+++ SATSolver.py
@@ -2,11 +2,12 @@ from helper_functions_SAT import *
 from txt2dimacs import *
 from visualization import *
 from printSudoku import check_sudoku
+import pandas as pd
 import time
 
-filepath="text-files/2 sudokus.txt"
+filepath="text-files/1000 sudokus.txt"
 
-sudokus=txt2strings(filepath)
+sudokus=txt2strings(filepath)[:2]
 
 node_metrics = {"T/F": [], "CP": [], "CN": [], "choice_depth": []}
 sudoku_metrics = {"num_steps": []}  # Number of steps is backtracks + 2 (or 1 if it only takes 1 step)
@@ -34,8 +35,22 @@ for sudoku in sudokus:
     # print(lit2truth)
     # truth2vis(lit2truth)
     # print("solution check successful: ", check_sudoku(lit2truth))
-    wait = input("PRESS ENTER TO CONTINUE.")
+    #wait = input("PRESS ENTER TO CONTINUE.")
 
+df = pd.DataFrame(node_metrics)
 # maxCP = 5, always the same
 # maxCP = 32, always the same
 
+
+df.to_pickle("text-files/dataframe1.txt") # save file
+import pandas as pd
+df = pd.read_pickle("text-files/dataframe1.txt")
+
+from sklearn import linear_model
+print(df)
+#import statsmodels.formula.api as sm
+#from pandas.stats.api import ols
+reg = linear_model.LinearRegression()
+reg.fit(df[['CP','CN','T/F']], df['choice_depth'])
+
+print(reg.coef_)
