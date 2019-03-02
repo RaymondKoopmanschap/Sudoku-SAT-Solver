451fc79052268d8822baba2cd138bfe6d2d9a903
RaymondKoopmanschap <raymond95@hotmail.nl>
2019-03-02 22:26:33 +0100

    minor change

--- SATSolver.py
+++ SATSolver.py
@@ -47,9 +47,7 @@ df = pd.read_pickle("text-files/dataframe1.txt")
 
 from sklearn import linear_model
 print(df)
-#import statsmodels.formula.api as sm
-#from pandas.stats.api import ols
 reg = linear_model.LinearRegression()
 reg.fit(df[['CP','CN','T/F']], df['choice_depth'])
 
-print(reg.coef_)
+print(reg.intercept_,reg.coef_)
