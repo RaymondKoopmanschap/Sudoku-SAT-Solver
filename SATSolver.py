4beac2180ed919324b70e20a56a788b5a4802592
RaymondKoopmanschap <raymond95@hotmail.nl>
2019-03-02 14:27:06 +0100

    now working through whole file of sudokus

--- SATSolver.py
+++ SATSolver.py
@@ -1,18 +1,31 @@
 from helper_functions_SAT import *
+from txt2dimacs import *
 from visualization import *
 import time
 
-seconds = time.time()
+filepath="text-files/1000 sudokus.txt"
 
-CNF = Dimacs2CNF("text-files/sudoku-dimacs.txt")
+sudokus=txt2strings(filepath)
 
-cl2truth, lit2truth, lit2cls, atomCount, litlist, choices = CNF
+for sudoku in sudokus:
+    seconds = time.time()
+    string2dimacs(sudoku,"text-files/sudoku-rules.txt","text-files/sudoku-dimacs_temp.txt")
 
-#temp = lit2truth.copy()
+    CNF = Dimacs2CNF("text-files/sudoku-dimacs_temp.txt")
 
-b = DP_algo_naive(CNF, litlist[0], 0)
-print(time.time() - seconds)  # 0.3 seconds
-print(b)
-print(lit2truth)
+    seconds = time.time()
+    print(time.time() - seconds)
+
+    cl2truth, lit2truth, lit2cls, atomCount, litlist, choices = CNF
+
+    #temp = lit2truth.copy()
+
+    b = DP_algo_naive(CNF, litlist[0], 0)
+    print(time.time() - seconds)  # 0.3 seconds
+    print(b)
+    print(lit2truth)
+
+    truth2vis(lit2truth)
+    wait = input("PRESS ENTER TO CONTINUE.")
 
 
--- txt2dimacs.py
+++ txt2dimacs.py
@@ -1,18 +1,24 @@
-with open("4x4.txt", "r") as file:
-    sudoku = file.readline()
 
-print(sudoku)
-row = 1
-column = 1
+def txt2strings(filepath):
+    with open(filepath, "r") as rf:
+        sudokus=list()
+        for sudoku in rf:
+            sudokus.append(sudoku)
+    return sudokus
 
-f = open("sudoku-Dimacs.txt", "w")
-for number in sudoku:
-    if number.isdigit():
-        n = str(row) + str(column) + str(number) + " 0\n"
-        f.write(n)
-    if column == 9:
-        row = row + 1
+def string2dimacs(sudoku,rulesfilepath,targetfilepath):
+    with open(targetfilepath, "w") as wf:
+        with open(rulesfilepath,"r") as rulesfile:
+            for line in rulesfile:
+                wf.write(line)
+        row = 1
         column = 1
-        continue
-    column = column + 1
-f.close()
+        for number in sudoku:
+            if number.isdigit():
+                n = str(row) + str(column) + str(number) + " 0\n"
+                wf.write(n)
+            if column == 9:
+                row = row + 1
+                column = 1
+                continue
+            column = column + 1
