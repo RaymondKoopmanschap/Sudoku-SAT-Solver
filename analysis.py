39953ebb89f48c8036a4a5059d14693e7fb856e8
RaymondKoopmanschap <raymond95@hotmail.nl>
2019-03-03 10:59:24 +0100

    minor changes

--- analysis.py
+++ analysis.py
@@ -2,6 +2,9 @@ import pandas as pd
 df = pd.read_pickle("text-files/dataframe1.txt")
 
 from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+poly = PolynomialFeatures()
+poly.fit_transform(df)
 print(df)
 reg = linear_model.LinearRegression()
 reg.fit(df[['CP','CN','T/F','choice_depth']], df['good_decision'])
